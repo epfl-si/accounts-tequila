76a5325819592b737c742e67347ffe5f161149b7
Dominique Quatravaux <dominique@quatravaux.org>
2016-02-29 08:19:10 +0100

    Remove dead code

--- accounts-tequila-common.js
+++ accounts-tequila-common.js
@@ -12,19 +12,6 @@ Tequila = {
   }
 };
 
-if (this.Iron) {
-  Iron.Router.plugins.tequila = function (router, options) {
-    router.onBeforeAction(function() {
-      if (!Meteor.userId()) {
-        this.render('tequila.login');  // TODO: Find a way to just do a redirect
-                                       // without doing any templating in the
-                                       // tequila package
-        return pause();
-      }
-    }, options);
-  };
-}
-
 Meteor.startup(function() {
   Meteor.defer(function() {
     if (Tequila.options.autoStart) {
