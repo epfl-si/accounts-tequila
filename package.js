b812bc416c29f6e9ea5a7569d1da63b42cbb2808
Dominique Quatravaux <dominique@quatravaux.org>
2016-10-04 09:07:14 +0200

    Bump version
    Also remove scaffolding

--- package.js
+++ package.js
@@ -1,17 +1,13 @@
 Package.describe({
   name: 'epfl:accounts-tequila',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
+  version: '0.1.0',
   summary: 'Log in to Web apps using EPFL\'s [Tequila](http://tequila.epfl.ch/)',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/epfl-sti/accounts-tequila',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
+  api.versionsFrom('METEOR@1.2.1');
   api.use('ecmascript');
   api.use('ejson');
   api.use('accounts-base');
